d7b2b8167171e514f0181582ad658f72dfd439b2
KAZUMA Ukyo <yaakaito@gmail.com>
2013-08-20 01:10:01 +0900

    Add Specs2 dependencies

--- src/main/g8/project/$name__Camel$Build.scala
+++ src/main/g8/project/$name__Camel$Build.scala
@@ -10,7 +10,15 @@ object $name;format="Camel"$Build extends Build {
       name := "$name$",
       organization := "$organization$",
       version := "$version$",
-      scalaVersion := "$scala_version$"
+      scalaVersion := "$scala_version$",
+      libraryDependencies := Seq(
+        "org.scala-lang" % "scala-library" % "2.10.2",
+        "org.specs2" %% "specs2" % "latest.integration" % "test",
+        "org.specs2" %% "specs2-scalaz-core" % "latest.integration" % "test",
+        "junit" % "junit" % "latest.integration" % "test",
+        "org.hamcrest" % "hamcrest-all" % "latest.integration" % "test",
+        "org.pegdown" % "pegdown" % "latest.integration" % "test"
+      )
       // add other settings here
     )
   )
